580fb967f1295277fa448c075bdb339a42a7e4d8
Adnan Choudhary <adnanchoudhary1@gmail.com>
2025-02-27 13:21:00 -0500

    removed max_files, just year partitioning

--- campd_visualizer/scripts/zip2parquet.py
+++ campd_visualizer/scripts/zip2parquet.py
@@ -86,13 +86,12 @@ def make_parquet_from_zip_files(
         format="parquet",
         partitioning=[
             "Year",
-            "State",
+            # "State",
         ],
         partitioning_flavor="hive",
         existing_data_behavior="overwrite_or_ignore",
-        min_rows_per_group = 10**5,
-        max_rows_per_group = 10**6,
-        max_open_files = 3000,
+        min_rows_per_group=10**6,
+        max_rows_per_group=10**7,
     )
     print(f"Partitioned dataset written to: {out_dir}")
 
